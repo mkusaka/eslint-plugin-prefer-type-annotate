cc9850049ac2259ee447eb195340009ae45aefa5
mkusaka <hinoshita1992@gmail.com>
2020-05-31 20:05:54 +0900

    publish

--- dist/rules/prefer-type-annotation.js
+++ dist/rules/prefer-type-annotation.js
@@ -1,7 +1,10 @@
 "use strict";
 exports.__esModule = true;
 exports.preferTypeAnnotation = void 0;
+var tslib_1 = require("tslib");
 var experimental_utils_1 = require("@typescript-eslint/experimental-utils");
+var tsutils_1 = require("tsutils");
+var ts = tslib_1.__importStar(require("typescript"));
 var createRule = experimental_utils_1.ESLintUtils.RuleCreator(function (ruleName) { return ruleName; });
 exports.preferTypeAnnotation = createRule({
     name: "prefer-type-annotate",
@@ -20,36 +23,43 @@ exports.preferTypeAnnotation = createRule({
     },
     defaultOptions: [],
     create: function (context) {
-        var _a;
-        var _b = experimental_utils_1.ESLintUtils.getParserServices(context), program = _b.program, esTreeNodeToTSNodeMap = _b.esTreeNodeToTSNodeMap;
+        var _a = experimental_utils_1.ESLintUtils.getParserServices(context), program = _a.program, esTreeNodeToTSNodeMap = _a.esTreeNodeToTSNodeMap;
+        function report(location, name) {
+            context.report({
+                node: location,
+                messageId: "preferTypeAnnotate",
+                data: { name: name }
+            });
+        }
         var checker = program.getTypeChecker();
+        function getTypeFlags(type) {
+            var flags = 0;
+            for (var _i = 0, _a = tsutils_1.unionTypeParts(type); _i < _a.length; _i++) {
+                var t = _a[_i];
+                flags |= t.flags;
+            }
+            return flags;
+        }
+        function isTypeFlagSet(type, flagsToCheck) {
+            var flags = getTypeFlags(type);
+            return (flags & flagsToCheck) !== 0;
+        }
+        function isTypeAnyType(type) {
+            return isTypeFlagSet(type, ts.TypeFlags.Any);
+        }
         // const sourceCode = context.getSourceCode();
         // const printer = (n: TSESTree.Node) => sourceCode.getText(n);
         // const filePath = context.getFilename();
-        return _a = {},
-            _a[experimental_utils_1.AST_NODE_TYPES.CallExpression] = function (node) {
-                console.log(node.callee);
-                if (node.callee.type === experimental_utils_1.AST_NODE_TYPES.MemberExpression &&
-                    node.callee.property.type === experimental_utils_1.AST_NODE_TYPES.Identifier) {
-                    var obj = node.callee.object;
-                    var prop = node.callee.property;
-                    var objTsNode = esTreeNodeToTSNodeMap.get(obj);
-                    var objType = checker.getTypeAtLocation(objTsNode);
-                    console.log(checker.typeToString(objType));
-                    if (checker.typeToString(objType) === "") {
-                        if (["next", "prev", "parseInt"].includes(prop.name)) {
-                            context.report({
-                                node: node,
-                                messageId: "preferTypeAnnotate",
-                                data: {
-                                    method: prop.name
-                                }
-                            });
-                        }
-                    }
-                }
-            },
-            _a;
+        return {
+            ArrowFunctionExpression: function (node) {
+                console.log("params: ", node.params);
+                var tsNode = esTreeNodeToTSNodeMap.get(node);
+                var nodeType = checker.getTypeAtLocation(tsNode);
+                console.log("objType: ", nodeType);
+                console.log("isTypeAnyType: ", isTypeAnyType(nodeType));
+                report(node);
+            }
+        };
     }
 });
 //# sourceMappingURL=prefer-type-annotation.js.map
--- src/rules/prefer-type-annotation.ts
+++ src/rules/prefer-type-annotation.ts
@@ -3,6 +3,10 @@ import {
   TSESTree,
   AST_NODE_TYPES
 } from "@typescript-eslint/experimental-utils";
+import {
+  unionTypeParts
+} from "tsutils"
+import * as ts from 'typescript';
 
 const createRule = ESLintUtils.RuleCreator((ruleName) => ruleName);
 
@@ -26,38 +30,110 @@ export const preferTypeAnnotation = createRule({
     const { program, esTreeNodeToTSNodeMap } = ESLintUtils.getParserServices(
       context
     );
+    function report(location: TSESTree.Node, name?: string): void {
+      context.report({
+        node: location,
+        messageId: "preferTypeAnnotate",
+        data: { name },
+      });
+    }
     const checker = program.getTypeChecker();
+
+    function getTypeFlags(type: ts.Type) {
+      let flags: ts.TypeFlags = 0;
+      for (const t of unionTypeParts(type)) {
+        flags |= t.flags;
+      }
+      return flags;
+    }
+
+    function isTypeFlagSet(type: ts.Type, flagsToCheck: ts.TypeFlags) {
+      const flags = getTypeFlags(type);
+
+      return (flags & flagsToCheck) !== 0;
+    }
+
+    function isTypeAnyType(type: ts.Type) {
+      return isTypeFlagSet(type, ts.TypeFlags.Any)
+    }
+
+    function checkParameters(params: TSESTree.Parameter[]): void {
+      for (const param of params) {
+        let annotationNode: TSESTree.Node | undefined;
+
+        switch (param.type) {
+          case AST_NODE_TYPES.AssignmentPattern:
+            annotationNode = param.left;
+            break;
+          case AST_NODE_TYPES.TSParameterProperty:
+            annotationNode = param.parameter;
+
+            // Check TS parameter property with default value like `constructor(private param: string = 'something') {}`
+            if (
+              annotationNode &&
+              annotationNode.type === AST_NODE_TYPES.AssignmentPattern
+            ) {
+              annotationNode = annotationNode.left;
+            }
+
+            break;
+          default:
+            annotationNode = param;
+            break;
+        }
+
+        if (annotationNode !== undefined && !annotationNode.typeAnnotation) {
+          // report(param, getNodeName(param));
+        }
+      }
+    }
 
     // const sourceCode = context.getSourceCode();
     // const printer = (n: TSESTree.Node) => sourceCode.getText(n);
     // const filePath = context.getFilename();
 
     return {
-      [AST_NODE_TYPES.CallExpression](node: TSESTree.CallExpression): void {
-        console.log(node.callee)
-        if (
-          node.callee.type === AST_NODE_TYPES.MemberExpression &&
-          node.callee.property.type === AST_NODE_TYPES.Identifier
-        ) {
-          const obj = node.callee.object;
-          const prop = node.callee.property;
-          const objTsNode = esTreeNodeToTSNodeMap.get(obj);
-          const objType = checker.getTypeAtLocation(objTsNode);
-
-          console.log(checker.typeToString(objType))
-          if (checker.typeToString(objType) === "") {
-            if (["next", "prev", "parseInt"].includes(prop.name)) {
-              context.report({
-                node,
-                messageId: "preferTypeAnnotate",
-                data: {
-                  method: prop.name,
-                },
-              });
-            }
-          }
-        }
+      ArrowFunctionExpression(node): void {
+        console.log("params: ", node.params)
+        const tsNode = esTreeNodeToTSNodeMap.get(node)
+        const nodeType = checker.getTypeAtLocation(tsNode)
+        console.log("objType: ", nodeType)
+        console.log("isTypeAnyType: ", isTypeAnyType(nodeType))
+        const paramstsNode = node.params.map(e => esTreeNodeToTSNodeMap.get(e));
+        const paramsnodeTypes = paramstsNode.map(e => checker.getTypeAtLocation(e))
+        console.log("objType: ", paramsnodeTypes)
+        console.log("isTypeAnyType: ", paramsnodeTypes.map(e => isTypeAnyType(e)))
+        report(node)
       },
+      // [AST_NODE_TYPES.ArrayPattern](node: TSESTree.LiteralExpression): void {
+      //   console.log("node: ", node)
+      //   const objTsNode = esTreeNodeToTSNodeMap.get(obj);
+      //   const objType = checker.getTypeAtLocation(objTsNode);
+
+      //   console.log("checker to string", checker.typeToString(objType))
+      //   if (
+      //     node.callee.type === AST_NODE_TYPES.MemberExpression &&
+      //     node.callee.property.type === AST_NODE_TYPES.Identifier
+      //   ) {
+      //     const obj = node.callee.object;
+      //     const prop = node.callee.property;
+      //     const objTsNode = esTreeNodeToTSNodeMap.get(obj);
+      //     const objType = checker.getTypeAtLocation(objTsNode);
+
+      //     console.log(checker.typeToString(objType))
+      //     if (checker.typeToString(objType) === "") {
+      //       if (["next", "prev", "parseInt"].includes(prop.name)) {
+      //         context.report({
+      //           node,
+      //           messageId: "preferTypeAnnotate",
+      //           data: {
+      //             method: prop.name,
+      //           },
+      //         });
+      //       }
+      //     }
+      //   }
+      // },
     };
   },
 })
