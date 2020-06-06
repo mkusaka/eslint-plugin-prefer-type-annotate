c56121e8bfea11410a95b1eb9a49d608f6da4627
mkusaka <hinoshita1992@gmail.com>
2020-06-07 00:54:12 +0900

    chore test introduce test [ci skip]

---
+++ test/utils/rules/prefer-type-annotation.test.ts
@@ -0,0 +1,32 @@
+import { preferTypeAnnotation } from "../../../src/rules/prefer-type-annotation";
+
+import { RuleTester } from "../test-utils";
+
+import path from "path";
+
+const ruleTester = new RuleTester({
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2015,
+    project: "tsconfig.test.json",
+    tsconfigRootDir: path.resolve(__dirname, "..", "..", ".."),
+  },
+});
+
+ruleTester.run("prefer-type-annotation", preferTypeAnnotation, {
+  valid: [
+    {
+      code: "const [a] = [1]",
+    },
+  ],
+  invalid: [
+    {
+      code: "const [a] = [1 as any]",
+      errors: [
+        {
+          messageId: "VariableDeclaratorArray",
+        },
+      ],
+    },
+  ],
+});
