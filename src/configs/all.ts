c769fc7eaa0f403a745a90a09c0a58d751d4ffd7
mkusaka <hinoshita1992@gmail.com>
2020-06-07 15:37:36 +0900

    fix: nest config path

--- src/configs/all.ts
+++ src/configs/all.ts
@@ -1,15 +1,17 @@
 export function ruleLeveler(level: "error" | "warn" | "off") {
   return {
-    "@mkusaka/prefer-type-annotate/array-pattern": level,
-    "@mkusaka/prefer-type-annotate/arrow-function-expression": level,
-    "@mkusaka/prefer-type-annotate/class-property": level,
-    "@mkusaka/prefer-type-annotate/function-declaration": level,
-    "@mkusaka/prefer-type-annotate/function-expression": level,
-    "@mkusaka/prefer-type-annotate/object-pattern": level,
-    "@mkusaka/prefer-type-annotate/ts-as-expression": level,
-    "@mkusaka/prefer-type-annotate/ts-index-signature": level,
-    "@mkusaka/prefer-type-annotate/ts-property-signature": level,
-    "@mkusaka/prefer-type-annotate/variable-declarator": level,
+    rules: {
+      "@mkusaka/prefer-type-annotate/array-pattern": level,
+      "@mkusaka/prefer-type-annotate/arrow-function-expression": level,
+      "@mkusaka/prefer-type-annotate/class-property": level,
+      "@mkusaka/prefer-type-annotate/function-declaration": level,
+      "@mkusaka/prefer-type-annotate/function-expression": level,
+      "@mkusaka/prefer-type-annotate/object-pattern": level,
+      "@mkusaka/prefer-type-annotate/ts-as-expression": level,
+      "@mkusaka/prefer-type-annotate/ts-index-signature": level,
+      "@mkusaka/prefer-type-annotate/ts-property-signature": level,
+      "@mkusaka/prefer-type-annotate/variable-declarator": level,
+    },
   };
 }
 
